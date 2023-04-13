208cfccb8569653bbe1654092e44edbf61c73f87
FrancoLelli <francogastonlelli@gmail.com>
2023-04-13 15:13:55 -0300

    BaSP-W05-2023: Add exercises of block one

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,28 @@
+console.log("-------------------------EXERCISE 1 : VARIABLES AND OPERATORS-------------------------");
+
+/* a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una
+3er variable. */
+
+console.log("-Exercise 1.a");
+var numberOne = 3;
+var numberTwo = 2;
+var result = numberOne + numberTwo;
+console.log(result);
+
+/* b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+
+console.log("-Exercise 1.b");
+var wordOne = "Hola";
+var wordTwo = "Franco";
+var result = wordOne + " " + wordTwo;
+console.log(result);
+
+/* c.Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+guardando el resultado de la suma en una 3er variable (utilizar length).*/
+
+console.log("-Exercise 1.c");
+
+var wordThree = "Independiente";
+var wordFour = "Racing";
+var result = wordThree.length + wordFour.length;
+console.log(result);
